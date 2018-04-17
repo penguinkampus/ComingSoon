16ae89a2d93f5dc9dc429a80ebe79fdeba111060
Penguin Kampus <1312501925@student.budiluhur.ac.id>
2018-04-17 21:57:18 +0700

    Commit File

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SUAIQ.COM: Maintenance..</title>
+
+    <meta property="og:title" content="SUAIQ.COM - A TECH SOLUTION & CREATIVE DIGITAL AGENCY"/>
+    <meta property="og:url" content="http://suaiq.com"/>
+    <meta property="og:image" content=""/>
+    <meta property="og:description" content="A TECH SOLUTION & CREATIVE DIGITAL AGENCY"/>
+
+    <meta name="twitter:site" content=""/>
+    <meta name="twitter:creator" content="Muhammad Iqbal Pamungkas"/>
+    <meta name="twitter:title" content="SUAIQ.COM - A TECH SOLUTION & CREATIVE DIGITAL AGENCY"/>
+    <meta name="twitter:url" content=""/>
+    <meta name="twitter:description" content="A TECH SOLUTION & CREATIVE DIGITAL AGENCY"/>
+    <meta name="twitter:image" content=""/>
+
+    <meta itemprop="name" content="SUAIQ.COM - A TECH SOLUTION & CREATIVE DIGITAL AGENCY"/>
+    <meta itemprop="description" content="A TECH SOLUTION & CREATIVE DIGITAL AGENCY"/>
+    <link href='img/logo-suaiq.png' rel='icon' type='image/x-icon'/>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/landing-page.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+</head>
+
+<body class="site">
+    <div class="overlay">
+        <!-- Header -->
+        <div class="container">
+            <div class="intro-message">
+                <h1>SUAIQ.com</h1>
+                <h2>Our website is currently under construction.</h2>
+                <hr class="intro-divider">
+                <h3>In the meantime, you can find us on these social media platforms.</br>
+                Stay tuned for something amazing!</h3>
+                <ul class="intro-social-buttons">
+                  <li>
+                      <a href="http://blog.suaiq.com" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-globe fa-fw"></i> <span class="network-name">Blog</span></a>
+                  </li>
+                  <li>
+                      <a href="https://www.linkedin.com/company/suaiq/" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
+                  </li>
+                  <li>
+                      <a href="https://github.com/penguinkampus" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                  </li>
+                  <li>
+                      <a href="https://www.instagram.com/suaiqtech/" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-instagram fa-fw"></i> <span class="network-name">Instagram</span></a>
+                  </li>
+                  <li>
+                      <a href="http://bit.ly/2oS9X33" target="_blank" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-fw"></i> <span class="network-name">Facebook</span></a>
+                  </li>
+                </ul>
+            </div>
+
+            <div class="intro-footer">
+                <p class="copyright text-muted small">Copyright &copy; 2017-2018. All Rights Reserved</p>
+            </div>
+        </div>
+        <!-- /.container -->
+    </div>
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
